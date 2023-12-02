8afd13a4232f555aae554cbf35a5cdd3dbd89bee
NyanLinHtet1999 <andydufresne323@gmail.com>
2023-12-02 12:52:45 +0630

    config

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // $user = User::all();
+    // $user->update([
+    //     'name' => 'Thet'
+    // ]);
+    // User::create([
+    //     'name'=> 'Admin5',
+    //     'email'=> 'admin5@gmail.com',
+    //     'password'=> 'password',
+    // ]);
+    // $user = User::find(9);
+    // dd($user->name);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
